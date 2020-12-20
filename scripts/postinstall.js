3848f058f19f02c3c858d5c8429ed3aefe00298b
devappd <devappd@outlook.com>
2020-12-19 22:30:56 -0500

    postinstall: Don't save emsdkPath if node_modules

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -5,23 +5,36 @@
 
 const GetValidatedEmsdkPath = require('../src/validate.js');
 const execSync = require('child_process').execSync;
+const path = require('path');
 
+// https://stackoverflow.com/questions/37521893/determine-if-a-path-is-subdirectory-of-another-in-node-js
+function isAncestorDir(papa, child) {
+    const papaDirs = papa.split(path.sep).filter(dir => dir!=='');
+    const childDirs = child.split(path.sep).filter(dir => dir!=='');
+
+    return papaDirs.every((dir, i) => childDirs[i] === dir);
+}
+
+// Do sanity checks on install path and print warning messages.
 const emsdkPath = GetValidatedEmsdkPath();
+// presumes <module_dir>/scripts
+const modulePath = path.resolve(path.join(__dirname, '..'));
 
-// Print the warning messages. Truthy if the path is valid.
+// Truthy if the path is valid.
 if (emsdkPath) {
-    // If emsdkPath is specified, save it to NPM config.
+    // If emsdkPath is specified, AND it does not have `node_modules` in it, then save the config.
     //
-    // If this path from the NPM config, I don't know if we can differentiate
-    // between the `npmrc` user/global files and the command line, because
-    // these are researched through the same call to `config()`.
+    // If this path was found by checking `config()`, I don't know if we can differentiate
+    // whether it was sourced from the command line, the user config, or the global config.
     //
     // Therefore, just save indiscriminately to the user config.
-    execSync(`npm config set emsdk "${emsdkPath}"`);
+    if (!isAncestorDir(modulePath, emsdkPath)) {
+        execSync(`npm config set emsdk "${emsdkPath}"`);
 
-    console.log(`
-Emscripten SDK installation path is set to:
+        console.log(`
+    Emscripten SDK installation path is set to:
 
-    ${emsdkPath}
+        ${emsdkPath}
 `.trimLeft());
+    }
 }
